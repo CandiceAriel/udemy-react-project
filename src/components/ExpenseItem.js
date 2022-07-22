210262d8946c17ec83a172d875a5c549dc2c3dde
Candice Ariel <candicewangsa@gmail.com>
2022-07-22 23:12:17 +0700

    Expense Item Styling + Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import '../assets/scss/components/ExpenseItem.scss';
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    const expenseDate = new Date(2021, 3, 28);
+    const expenseTitle = "Car Insurance";
+    const expensePrice = 294.67;
+
+    return (
+        <div className= "c-expenseitem">
+            <div>{expenseDate.toISOString()}</div>
+            <div className = "c-expenseitem-description"> 
+                <h2 className = "c-expenseitem-description-title">{expenseTitle}</h2>
+                <div className = "c-expenseitem-description-price">${expensePrice}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
